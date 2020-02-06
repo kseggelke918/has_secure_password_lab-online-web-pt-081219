0ce31af6b2336eff94f05c27686d9488def45467
Katie Seggelke <katie.seggelke@gmail.com>
2020-02-06 23:13:55 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,10 +7,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(name: params[:user][:name])
-    
-    # user = user.try(:authenticate, params[:user][:password])
     if user && user.authenticate(params[:user][:password])
-      # redirect_to sessions_new_path unless user 
       session[:user_id] = user.id 
       @user = user 
       redirect_to sessions_homepage_path 
@@ -20,6 +17,6 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session.delete :user_id 
+    session.delete :user_id
   end
 end
